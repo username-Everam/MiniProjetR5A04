c22bd7ae573beebbad78e6e674a04dfccc1411de
username-Everam <simsimi.armand@gmail.com>
2024-12-02 08:54:30 +0100

    Truc de sofa

--- element_commun1.py
+++ element_commun1.py
@@ -1,3 +1,5 @@
+import time;
+import numpy as np
 #Question 2 :
 
 def element_commun1(L1, L2):
@@ -11,3 +13,18 @@ def element_commun1(L1, L2):
         else:
             L2.remove(m2)
     return float('nan')
+
+def chronometre_element_commun1(L1,L2):
+    time_debut=time.time()
+    element_commun1(L1,L2)
+    time_fin=time.time()
+    return time_fin-time_debut
+
+def test_element_chrono(n):
+    tableau_res=[]
+    for i in range(10):
+        Nmax=1000
+        liste1 = np.random.randint(Nmax,size=n).tolist()
+        liste2 = np.random.randint(Nmax,size=n).tolist()
+        tableau_res.append(chronometre_element_commun1(liste1,liste2))
+    return tableau_res
--- mainTest.py
+++ mainTest.py
@@ -1,11 +1,19 @@
-#Question 3 :
+#Question 3 et 4 :
 
 import element_commun1 as com
 import numpy as np
 
-n=10
-Nmax=100
-liste1 = np.random.randint(Nmax,size=n).tolist()
-liste2 = np.random.randint(Nmax,size=n).tolist()
+def test_element_commun1() :
+    for i in range(10):
+        n=10
+        Nmax=100
+        liste1 = np.random.randint(Nmax,size=n).tolist()
+        liste2 = np.random.randint(Nmax,size=n).tolist()
+        liste_originelle1 = liste1.copy()
+        liste_originelle2 = liste2.copy()
+        liste_originelle1.sort()
+        liste_originelle2.sort()
+        x = com.element_commun1(liste1,liste2)
+        print(x, liste_originelle1, liste_originelle2)
 
-print(com.element_commun1(liste1,liste2))
+print(com.test_element_chrono(1000))
