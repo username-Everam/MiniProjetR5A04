e29e34c0f2182366ee88d754f54cd341fd926022
username-Everam <simsimi.armand@gmail.com>
2024-12-02 08:33:52 +0100

    Update mainTest.py

--- mainTest.py
+++ mainTest.py
@@ -1,1 +1,11 @@
-print("Hello world")
+#Question 3 :
+
+import element_commun1 as com
+import numpy as np
+
+n=10
+Nmax=100
+liste1 = np.random.randint(Nmax,size=n).tolist()
+liste2 = np.random.randint(Nmax,size=n).tolist()
+
+print(com.element_commun1(liste1,liste2))
