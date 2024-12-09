0baeffe464c73023ff176e8680204dda1041c645
username-Everam <simsimi.armand@gmail.com>
2024-12-09 09:08:25 +0100

    add tas

--- element_commun1.py
+++ element_commun1.py
@@ -1,5 +1,6 @@
 import time;
 import numpy as np
+import heapq
 #Question 2 :
 
 def element_commun1(L1, L2):
@@ -14,8 +15,28 @@ def element_commun1(L1, L2):
             L2.remove(m2)
     return float('nan')
 
+def element_commun2(L1, L2):
+    while L1 and L2:
+        m1 = L1[0]
+        m2 = L2[0]
+        if m1 == m2:
+            return m1
+        elif m1 < m2:
+            heapq.heappop(L1)
+        else:
+            heapq.heappop(L2)
+    return float('nan')
+
 def chronometre_element_commun1(L1,L2):
     time_debut=time.time()
     element_commun1(L1,L2)
+    time_fin=time.time()
+    return time_fin-time_debut
+
+def chronometre_element_commun2(L1,L2):
+    heapq.heapify(L1)
+    heapq.heapify(L2)
+    time_debut=time.time()
+    element_commun2(L1,L2)
     time_fin=time.time()
     return time_fin-time_debut
--- mainTest.py
+++ mainTest.py
@@ -27,10 +27,49 @@ def test_element_chrono(n):
         tableau_temps.append(temps)
     return tableau_temps
 
-def afficher_graph(n):
+def afficher_graph1(n):
     tableau=test_element_chrono(n)
     x=[*range(0,n,10)]
     plt.scatter(x,tableau)
     plt.show()
     
-afficher_graph(5000)
+def test_element_commun2() :
+    for i in range(10):
+        n=10
+        Nmax=100
+        liste1 = np.random.randint(Nmax,size=n).tolist()
+        liste2 = np.random.randint(Nmax,size=n).tolist()
+        liste_originelle1 = liste1.copy()
+        liste_originelle2 = liste2.copy()
+        liste_originelle1.sort()
+        liste_originelle2.sort()
+        x = com.element_commun2(liste1,liste2)
+        print(x, liste_originelle1, liste_originelle2)
+
+def test_element_chrono(n):
+    tableau_temps=[]
+    for i in range(0 ,n, 10):
+        Nmax=10000000000
+        liste1 = np.random.randint(Nmax,size=i).tolist()
+        liste2 = np.random.randint(Nmax,size=i).tolist()
+        temps = com.chronometre_element_commun2(liste1,liste2)
+        tableau_temps.append(temps)
+    return tableau_temps
+
+def afficher_graph2(n):
+    tableau=test_element_chrono(n)
+    x=[*range(0,n,10)]
+    plt.scatter(x,tableau)
+    plt.show()
+
+def afficher_graph_comparaison(n):
+    tableau1=test_element_chrono(n)
+    tableau2=test_element_chrono(n)
+    x=[*range(0,n,10)]
+    plt.scatter(x,tableau1)
+    plt.scatter(x,tableau2)
+    plt.show()
+
+afficher_graph1(50000)
+afficher_graph2(50000)
+afficher_graph_comparaison(50000)
